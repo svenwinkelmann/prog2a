f10161c3a3ae6ac9c03ec5548c10ee0dab2d8b3f
Sven Winkelmann <sven.winkelmann@th-nuernberg.de>
2023-05-30 16:33:57 +0200

    Extended graph example

--- src/graphs/Vertex.java
+++ src/graphs/Vertex.java
@@ -8,6 +8,8 @@ public class Vertex {
 
     // this is the data element of our vertex, can be any data type, complex ones too
     protected String name;
+    // this label contains the marker for graph traversal (e.g. VISITED, UNEXPLORED, ...)
+    protected int label;
 
     public Vertex(String name) {
         this.name = name;
@@ -15,6 +17,14 @@ public class Vertex {
 
     public String getName() {
         return this.name;
+    }
+
+    public int getLabel() {
+        return this.label;
+    }
+
+    public void setLabel(int label) {
+        this.label = label;
     }
 
     public String toString() {
