013f7344a47780af4f5700edb9b27955217ff7ec
Sven Winkelmann <sven.winkelmann@th-nuernberg.de>
2025-03-31 22:33:32 +0200

    Mutability: modify_in_loops

--- src-python/1-DatatypesStructures/Mutability.py
+++ src-python/1-DatatypesStructures/Mutability.py
@@ -18,6 +18,21 @@ def immutable():
     print("ID after append: ", id(my_string))
 
 
+def modify_in_loops():
+    print("\nmodify_in_loops()")
+    my_list1 = [1, 2, 3, 4, 5, 6]
+
+    for i in my_list1.copy():
+        if i % 2 != 0:
+            my_list1.remove(i)
+    print("my_list1: ", my_list1)
+
+    my_list2 = [1, 2, 3, 4, 5, 6]
+    my_even_list = [x for x in my_list2 if x % 2 == 0]
+    print("my_even_list: ", my_even_list)
+
+
 if __name__ == "__main__":
-    mutable()
-    immutable()
+    #mutable()
+    #immutable()
+    modify_in_loops()
