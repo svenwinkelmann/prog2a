7e62daa7079b9f397791b6dff108b1aaf2926435
Sven Winkelmann <sven.winkelmann@th-nuernberg.de>
2025-03-31 11:59:38 +0200

    Associative Container

--- src-python/1-DatatypesStructures/Typing.py
+++ src-python/1-DatatypesStructures/Typing.py
@@ -1,12 +1,12 @@
 
 if __name__ == "__main__":
     sophia = 17
-    #sophia = Turtle()
-    sophia = 'Hello World!'
     sophia = 3.4
+    sophia = 'Hello World!'
 
     b = 5
-    #c = b + sophia
+    c = b + sophia
+    print(f"c: {c}")
 
     print(type(sophia))
     print(type(b))
