4fb8ff31a53579b004aabdff70bb3cbb0e75f257
Sven Winkelmann <sven.winkelmann@th-nuernberg.de>
2025-04-22 23:02:45 +0200

    PrintList

---
+++ src-python/5-Recursion/PrintList.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+def print_list(queue):
+    if len(queue) <= 0:  # base case
+        return
+
+    i = queue.popleft()
+    print(f"{i}, ", end='')  # insertion order
+    print_list(queue)  # recursive case
+    print(f"{i}, ", end='')  # reverse order
+
+def main():
+    queue = deque([4, 9, 2, 10, 34, 76])
+    print_list(queue)
+
+if __name__ == "__main__":
+    main()
